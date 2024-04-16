0c55cf087ad67a282e65ef9e04b2badf02af8612
junaid-12junaid <junaidullakhan619@gmail.com>
2024-04-16 19:30:56 +0530

    added the re- rendering using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 import { CreateTodo } from './components/CreateTodo'
 import { Todos } from './components/Todos'
 function App() {
 let [todo,setTodo]=useState([])
 
-// fetch("http://localhost:3000/todos").then(async(res)=>{
-//   let json=await res.json()
-//   console.log(json)
-//   setTodo(json.result)
-// })
+
+useEffect(()=>{
+fetch("http://localhost:3000/todos").then(async(res)=>{
+  let json=await res.json()
+  console.log(json)
+  setTodo(json.result)
+})
+},[])
+
+
 
   return (
    <div>
